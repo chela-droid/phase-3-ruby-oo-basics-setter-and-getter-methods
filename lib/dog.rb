0259bee37259942d867c09e215690127e2c83b9d
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-02-08 23:22:08 +0300

    setter

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
-class Dog
 
+class Dog
+    # attr_reader : name
+    def name=(name)
+        @name =name
+    end
+
+    def name
+      @name
+    end
+  
+
+    def breed
+      @breed
+    end
+
+    # attr_
+    def breed=(breed)
+      @breed =breed
+  end
+    # fido =Dog.new
+    # fido.name=('fido')
+
+    
+    
 end
+#  d1= Dog.new('Fido'
+ 
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
+    def name=(name)
+        @name=name
+    end
 
+    def name
+        @name
+    end
+
+    def job
+        @job
+      end
+  
+      # attr_
+      def job=(job)
+        @job =job
+      end
+
+    
+ 
 end
+
+
+
+
+
+
