71ba2f038659f9e358ae101a2a46320e5fd41255
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-02-09 00:33:03 +0300

    commit tree 19220a086982d238be292e728da16de6d922c01c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,46 @@
 
-class Dog
-    # attr_reader : name
-    def name=(name)
-        @name =name
-    end
+# class Dog
+#     # attr_reader : name
+#     def name=(name)
+#         @name =name
+#     end
 
-    def name
-      @name
-    end
+#     def name
+#       @name
+#     end
   
 
-    def breed
-      @breed
-    end
+#     def breed
+#       @breed
+#     end
 
-    # attr_
-    def breed=(breed)
-      @breed =breed
-  end
-    # fido =Dog.new
-    # fido.name=('fido')
+#     # attr_
+#     def breed=(breed)
+#       @breed =breed
+#   end
+    
 
     
     
-end
-#  d1= Dog.new('Fido'
+# end
+# #  d1= Dog.new('Fido'
  
 
+class Dog
+  # setter 
+  def name=(name)
+    @name=name
+  end
+  # getter
+  def name
+    @name
+  end
+# setter
+  def breed=(breed)
+    @breed =breed
+  end
+# getter
+  def breed
+    @breed
+  end
+end 
--- lib/person.rb
+++ lib/person.rb
@@ -1,27 +1,43 @@
+# class Person
+#     def name=(name)
+#         @name=name
+#     end
+
+#     def name
+#         @name
+#     end
+
+#     def job
+#         @job
+#       end
+  
+#       # attr_
+#       def job=(job)
+#         @job =job
+#       end
+
+# end
+
+
+
 class Person
+    # setter
     def name=(name)
         @name=name
     end
 
+    # getter
     def name
         @name
     end
 
-    def job
-        @job
-      end
-  
-      # attr_
       def job=(job)
-        @job =job
+          @job=job
       end
 
-    
- 
+      def job
+          @job
+      end
 end
-
-
-
-
 
 
